2b5ef857158eb8b5dc7340345b7c41e4873e64a0
daiyunhang <18810945181@163.com>
2019-09-22 14:30:44 +0800

    高阶组件完成

--- src/components/KForm.jsx
+++ src/components/KForm.jsx
@@ -13,10 +13,46 @@ function KFormCreate(Comp) {
 
         handleChange = (e) => {
             const { name, value } = e.target;
-            this.setState({ [name]: value });
-            console.log(name,value,'++++++++++++++++++');
+            this.setState({ [name]: value }, () => {
+                // 确保值发生变化再校验
+                this.validateField(name)
+            });
+            console.log(name, value, '++++++++++++++++++');
+
+        };
+
+        validateField = field => {
+            // 1.获取校验规则
+            const rules = this.option[field].rules;
+
+            // 任意一项失败返回false
+            const ret = !rules.some(rule => {
+                if (rule.required) {
+                    if (!this.state[field]) {
+                        // 校验失败
+                        this.setState({
+                            [field + 'Message']: rule.message
+                        })
+                        return true;
+                    }
+                }
+            })
+            if (ret) {
+                this.setState({
+                    [field + 'Message']: ''
+                })
+            }
+            return ret;
         }
 
+        // 校验所有字段
+        validate = cb => {
+            const rets = Object.keys(this.option).map(field =>
+                this.validateField(field)
+            );
+            const ret = rets.every(v => v == true);
+            cb(ret, this.state);
+        }
 
         // 创建input包装器
         getFieldDec = (field, option) => {
@@ -29,25 +65,44 @@ function KFormCreate(Comp) {
                         value: this.state[field] || "",
                         onChange: this.handleChange
                     })}
+                    {/* 校验错误信息 */}
+                    {this.state[field+'Message'] && (
+                        <p style={{color:'red'}}>{this.state[field+'Message']}</p>
+                    )}
                 </div>
             )
         }
         render() {
-            return <Comp getFieldDec={this.getFieldDec} />
+            return <Comp getFieldDec={this.getFieldDec} validate={this.validate} />
         }
     }
 }
 
 @KFormCreate
 class KForm extends Component {
+    onSubmit = () => {
+        console.log("submit");
+        // 校验所有项
+        this.props.validate((isValid,data)=>{
+            if(isValid){
+                //提交登录
+                console.log("登录:",data);
+            }else{
+                alert("校验失败");
+            }
+        });
+    }
     render() {
         const { getFieldDec } = this.props;
-        return(
-            <div>
-                {getFieldDec("uname", {
+        return (
+            <div style={{display:'flex',alignItems:'center'}}>
+                用户名:{getFieldDec("uname", {
                     rules: [{ required: true, message: "用户名必填" }]
                 })(<Input />)}
-                <Button>登录</Button>
+                密码:{getFieldDec("pwd", {
+                    rules: [{ required: true, message: "密码必填" }]
+                })(<Input />)}
+                <Button onClick={this.onSubmit}>登录</Button>
             </div>
         )
     }
